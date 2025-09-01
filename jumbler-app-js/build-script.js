36f40410f0c0b1ce7a8ed57fe0b5b3fd08065d26
Michael Glavitsch <michael.glavitsch@gmail.com>
2025-09-01 14:21:00 +0200

    create distDir if needed

--- jumbler-app-js/build-script.js
+++ jumbler-app-js/build-script.js
@@ -9,7 +9,13 @@ const delRegex = /^THIRD-PARTY-LICENSES(?:\.dev|\.[a-f0-9]{8})?\.txt$/;
 const repRegex = /THIRD-PARTY-LICENSES(?:\.dev|\.[a-f0-9]{8})?\.txt/g;
 const indexHtmlFile = path.join(distDir, "index.html");
 
-// 1) Delete old files starting with THIRD-PARTY-LICENSES and matching the regex
+// 1) Create distDir if needed
+if (!fs.existsSync(distDir)) {
+    fs.mkdirSync(distDir);
+    console.log(`Created missing directory: ${distDir}`);
+}
+
+// 2) Delete old files starting with THIRD-PARTY-LICENSES and matching the regex
 fs.readdirSync(distDir).forEach(file => {
     if (delRegex.test(file)) {
         fs.unlinkSync(path.join(distDir, file));
@@ -17,21 +23,21 @@ fs.readdirSync(distDir).forEach(file => {
     }
 });
 
-// 2) execute webpack --mode=production
+// 3) execute webpack --mode=production
 console.log("Running webpack build...");
 execSync("webpack --mode=production", { stdio: "inherit" });
 
-// 3) Read prod license file and derive hash
+// 4) Read prod license file and derive hash
 const licenseFile = path.join(distDir, prodLicenseFile);
 const licenseText = fs.readFileSync(licenseFile, "utf-8");
 const hash = crypto.createHash("md5").update(licenseText).digest("hex").slice(0, 8);
 
-// 4) Copy prod license file to new hashed license file
+// 5) Copy prod license file to new hashed license file
 const hashedLicenseFile = `THIRD-PARTY-LICENSES.${hash}.txt`;
 fs.copyFileSync(licenseFile, path.join(distDir, hashedLicenseFile));
 console.log(`Copied ${prodLicenseFile} → ${hashedLicenseFile}`);
 
-// 5) adapt dist/index.html
+// 6) adapt dist/index.html
 let indexHtml = fs.readFileSync(indexHtmlFile, "utf-8");
 indexHtml = indexHtml.replace(repRegex, hashedLicenseFile);
 fs.writeFileSync(indexHtmlFile, indexHtml, "utf-8");
